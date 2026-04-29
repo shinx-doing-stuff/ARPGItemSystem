63f5f139cd1b1207be0a943028c17f06a1355603
shinx-doing-stuff <nguongquang@gmail.com>
2026-04-29 11:25:03 +0200

    feat: implement UISystem replacing vanilla reforge layer with ARPG panel

--- Common/Systems/UISystem.cs
+++ Common/Systems/UISystem.cs
@@ -1,10 +1,54 @@
+using System.Collections.Generic;
 using ARPGItemSystem.Common.UI;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.UI;
 
 namespace ARPGItemSystem.Common.Systems
 {
     public class UISystem : ModSystem
     {
+        private UserInterface _reforgeInterface;
         internal ReforgePanel Panel;
+
+        public override void Load()
+        {
+            if (Main.netMode != NetmodeID.Server)
+            {
+                Panel = new ReforgePanel();
+                Panel.Activate();
+                _reforgeInterface = new UserInterface();
+                _reforgeInterface.SetState(Panel);
+            }
+        }
+
+        public override void UpdateUI(GameTime gameTime)
+        {
+            if (Main.InReforgeMenu)
+                _reforgeInterface?.Update(gameTime);
+        }
+
+        public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
+        {
+            int index = layers.FindIndex(l => l.Name == "Vanilla: Reforge Menu");
+            if (index < 0)
+            {
+                Mod.Logger.Warn("[ARPGItemSystem] Could not find 'Vanilla: Reforge Menu' interface layer.");
+                return;
+            }
+
+            layers[index] = new LegacyGameInterfaceLayer(
+                "ARPGItemSystem: Reforge Panel",
+                () =>
+                {
+                    if (Main.InReforgeMenu)
+                        _reforgeInterface.Draw(Main.spriteBatch, new GameTime());
+                    return true;
+                },
+                InterfaceScaleType.UI
+            );
+        }
     }
 }
