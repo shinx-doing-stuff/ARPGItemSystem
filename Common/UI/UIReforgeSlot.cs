06269ef655b20055641bf5fb212b8beeff2ab249
shinx-doing-stuff <nguongquang@gmail.com>
2026-04-29 14:21:08 +0200

    fix: explicit swap logic in DrawSelf (draw phase) - mouseLeft&&mouseLeftRelease = first click frame

--- Common/UI/UIReforgeSlot.cs
+++ Common/UI/UIReforgeSlot.cs
@@ -1,6 +1,8 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
 using Terraria.UI;
 
 namespace ARPGItemSystem.Common.UI
@@ -22,12 +24,31 @@ namespace ARPGItemSystem.Common.UI
             if (ContainsPoint(Main.MouseScreen))
             {
                 Main.LocalPlayer.mouseInterface = true;
-                // ItemSlot.Handle processes clicks (checks mouseLeft && mouseLeftRelease
-                // internally) and swaps SlotItem with Main.mouseItem when clicked.
-                ItemSlot.Handle(ref SlotItem, ItemSlot.Context.InventoryItem);
+
+                // mouseLeft = currently pressed
+                // mouseLeftRelease = was NOT pressed last update
+                // Both true = first frame of press = a click
+                bool isClick = Main.mouseLeft && Main.mouseLeftRelease;
+                bool canPlace = Main.mouseItem.IsAir || Main.mouseItem.maxStack == 1;
+
+                if (isClick && canPlace)
+                {
+                    Item temp = Main.mouseItem;
+                    Main.mouseItem = SlotItem;
+                    SlotItem = temp;
+                    SoundEngine.PlaySound(SoundID.Grab);
+                    // Consume so DrawInventory doesn't also process this click
+                    Main.mouseLeft = false;
+                }
             }
 
+            // Draw-only: suppress mouse so ItemSlot.Draw doesn't also try to interact
+            int mx = Main.mouseX, my = Main.mouseY;
+            Main.mouseX = -9999;
+            Main.mouseY = -9999;
             ItemSlot.Draw(spriteBatch, ref SlotItem, ItemSlot.Context.InventoryItem, pos);
+            Main.mouseX = mx;
+            Main.mouseY = my;
         }
     }
 }
