5833dcb74f73786eb6ffaaf1a3950ca9c7c2607c
shinx-doing-stuff <nguongquang@gmail.com>
2026-04-29 13:24:58 +0200

    feat: centered panel with suppress + UIReforgeSlot, item return on close

--- Common/Systems/UISystem.cs
+++ Common/Systems/UISystem.cs
@@ -13,6 +13,7 @@ namespace ARPGItemSystem.Common.Systems
         private UserInterface _reforgeInterface;
         internal ReforgePanel Panel;
         private GameTime _lastGameTime = new GameTime();
+        private bool _reforgeWasOpen;
 
         public override void Load()
         {
@@ -37,12 +38,54 @@ namespace ARPGItemSystem.Common.Systems
             int inventoryIndex = layers.FindIndex(l => l.Name == "Vanilla: Inventory");
             if (inventoryIndex < 0) return;
 
-            layers.Insert(inventoryIndex + 1, new LegacyGameInterfaceLayer(
+            // Suppress vanilla reforge slot/button for the inventory draw only.
+            layers.Insert(inventoryIndex, new LegacyGameInterfaceLayer(
+                "ARPGItemSystem: Suppress Vanilla Reforge",
+                () =>
+                {
+                    _reforgeWasOpen = Main.InReforgeMenu;
+                    if (_reforgeWasOpen) Main.InReforgeMenu = false;
+                    return true;
+                },
+                InterfaceScaleType.UI
+            ));
+
+            layers.Insert(inventoryIndex + 2, new LegacyGameInterfaceLayer(
                 "ARPGItemSystem: Reforge Panel",
                 () =>
                 {
-                    if (Main.InReforgeMenu)
+                    if (!_reforgeWasOpen) return true;
+
+                    if (Main.playerInventory)
+                    {
+                        Main.InReforgeMenu = true;
                         _reforgeInterface.Draw(Main.spriteBatch, _lastGameTime);
+                    }
+                    else
+                    {
+                        // Return held item when menu closes (ESC / NPC walked away).
+                        // If vanilla already returned it, reforgeItem.IsAir is true and we skip.
+                        if (!Main.reforgeItem.IsAir)
+                        {
+                            if (Main.mouseItem.IsAir)
+                            {
+                                Main.mouseItem = Main.reforgeItem;
+                            }
+                            else
+                            {
+                                for (int i = 0; i < Main.LocalPlayer.inventory.Length; i++)
+                                {
+                                    if (Main.LocalPlayer.inventory[i].IsAir)
+                                    {
+                                        Main.LocalPlayer.inventory[i] = Main.reforgeItem;
+                                        break;
+                                    }
+                                }
+                            }
+                            Main.reforgeItem = new Item();
+                        }
+                        Main.InReforgeMenu = false;
+                    }
                     return true;
                 },
                 InterfaceScaleType.UI
